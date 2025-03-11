d43fd02651412ff039eb265f66c59a0cb0e3d4d0
Rainchus <rainmachus@gmail.com>
2025-03-10 23:21:46 -0500

    fix include error

--- patches/temp.c
+++ patches/temp.c
@@ -49,7 +49,7 @@ RECOMP_PATCH void func_global_asm_806F3760(s16 map) {
     void *autowalkFile;
 
     autowalkFile = getPointerTableFile(TABLE_21_AUTOWALKS, map, 1, 0);
-    //recomp_printf("autowalkFile loaded\n");
+    recomp_printf("autowalkFile loaded\n");
     func_global_asm_806F4528(autowalkFile);
     if (autowalkFile) {
         func_global_asm_8066B434(autowalkFile, 0x4C, 0x56);
